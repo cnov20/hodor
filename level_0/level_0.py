ae4fd33a5b2964a91a4fb33cd1bb74c1088cd66b
cnov20 <128@holbertonschool.com>
2017-05-17 23:52:56 +0000

    Updated for pep8 styling and deleted seemingly unnecessary q value from headers

--- level_0/level_0.py
+++ level_0/level_0.py
@@ -2,8 +2,8 @@
 import requests
 
 URL = 'http://54.221.6.249/level0.php'
-headers = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
+headers = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp'
 payload = {'id': '128', 'holdthedoor': 'submit'}
 
-for i in range (1024):
+for i in range(1024):
     r = requests.post(URL, data=payload)
