f9206238d114b52b119520fa9fc936889e0fb0f6
cnov20 <128@holbertonschool.com>
2017-05-17 15:57:19 +0000

    Successfully voted on site - 1024 times

--- level_0/level_0.py
+++ level_0/level_0.py
@@ -1,1 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/python
+import requests
+
+URL = 'http://54.221.6.249/level0.php'
+headers = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
+payload = {'id': '128', 'holdthedoor': 'submit'}
+
+for i in range (1024):
+    r = requests.post(URL, data=payload)
